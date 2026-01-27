01c9b66bb52e8ae689fa0098d874b7e8ed1e6d55
Skamik <skassamikolaj@gmail.com>
2026-01-27 21:09:52 +0100

    Cleanup unused scripts

--- test-betclic-connection.mjs
+++
@@ -1,7 +0,0 @@
-import { fetchLeagueMatches } from './src/scrapers/bookmakers/betclic/navigation.js';
-
-(async () => {
-  console.log('Testing Betclic listing endpoint...');
-  const result = await fetchLeagueMatches('premier-league');
-  console.log('Result:', result ? result.length + ' bytes' : 'null');
-})();
